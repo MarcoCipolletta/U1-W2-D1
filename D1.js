a08d110e091e29de9c20d9396dbc83e51aff144a
MarcoCipolletta <marcocipolletta1998@gmail.com>
2024-08-19 16:43:45 +0200

    es7

--- D1.js
+++ D1.js
@@ -76,3 +76,10 @@ console.log(4 - x);
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+
+let name1 = "john";
+let name2 = "John";
+
+console.log("name1 e name2 sono uguali?", name1 === name2);
+
+console.log(name1.toLowerCase === name2.toLowerCase);
